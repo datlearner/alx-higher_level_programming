c6096cf80a34bc7b14593f30620bca38be4fe15c
Kundai <kundaimuchemwa456@gmail.com>
2023-11-27 23:06:11 -0800

    Technical interview preparation:

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,11 +9,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * 
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -21,4 +22,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
