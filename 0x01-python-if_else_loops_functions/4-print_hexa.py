9f66ae9e1d8e20e41d593cf12fb6e94d18fde944
Kundai <kundaimuchemwa456@gmail.com>
2023-11-28 23:31:04 -0800

    prints all numbers

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-for i in range(99):
-    print("{} = {}".format(i, hex(i)))
+def myFunction(number):
+    nums = '0123456789abcdef'
+    t_n = number // 16
+    sin_gle = number % 16
+    if t_n > 0:
+        return nums[t_n] + nums[sin_gle]
+    else:
+        return nums[sin_gle]
+if __name__ == '__main__':
+    for number in range(99):
+        print('{:d} = 0x{}'.format(number, myFunction(number)))
