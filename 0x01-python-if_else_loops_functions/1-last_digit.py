3f8b4bb523a399c03b7b069f75b57c911f1d44cb
Kundai <kundaimuchemwa456@gmail.com>
2023-11-28 22:57:12 -0800

    assign a random signed number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_num = abs(number) % 10
-print('last digit of', number, 'is', last_num, end=' ')
+if number < 0:
+    last_num = ((number * -1) % 10) * -1
+else:
+    last_num = number % 10
+print(f'Last digit of {number:d} is {last_num:d} and is', end=' ')
 if last_num > 5:
-    print('and is greater than 5')
+    print('greater than 5')
 elif last_num == 0:
-    print('and of 0')
-elif last_num < 6 and last_num != 0:
-    print('and of less than 6 and not 0')
+    print('0')
+else:
+    print('less than 6 and not 0')
