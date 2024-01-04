a34f748509d881613a11530ee5dbf40a5fb2fe41
Kundai <kundaimuchemwa456@gmail.com>
2024-01-04 06:40:02 -0800

    empty class Rectangle that defines a rectangle:

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+""" empty class Rectangle that defines a rectangle
+"""
+
 
 def Rectangle(self):
-    """
-     empty class Rectangle that defines a rectangle:
-    """
+    """empty class"""
     pass
