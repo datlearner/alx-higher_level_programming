fce496681ea48e748aee4534be829ee5d1ed2090
datlearner <kundaimuchemwa456@gmail.com>
2024-05-02 15:48:53 +0200

    added  all taskes once

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,26 @@
+function sayHello () {
+  const lang = $('INPUT#language_code').val();
+  $.ajax({
+    type: 'POST',
+    url: 'https://fourtonfish.com/hellosalut/?lang=' + lang,
+    success: (translation) => {
+      $('DIV#hello').text(translation.hello);
+    },
+    error: () => {
+      console.log('Error loading orders');
+    }
+  });
+}
+
+window.onload = () => {
+  $('INPUT#btn_translate').focus(() => {
+    $('INPUT#btn_translate').keypress((key) => {
+      if (key.which === 13) {
+        sayHello();
+      }
+    });
+    $('INPUT#btn_translate').click(() => {
+      sayHello();
+    });
+  });
+};
